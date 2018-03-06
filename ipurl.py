5913076d46fbe847a9f858f932500612f2b09fe5
xxxHolic3 <33705429+xxxHolic3@users.noreply.github.com>
2018-03-06 08:52:17 +0800

    Add files via upload

---
+++ ipurl.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+from socket import *
+import sys
+
+print (" _____ _____            _ ")
+print ("|_   _|  __ \          | |")
+print ("  | | | |__) |   _ _ __| |")
+print ("  | | |  ___/ | | | '__| |")
+print (" _| |_| |   | |_| | |  | |")
+print ("|_____|_|    \__,_|_|  |_|")
+print ("[*] Version 1")
+print ("[*] @Falcon")
+print ("[*] This software is under GNU General Public License v3.0")                         
+                          
+host = input("\n[*] Target url : ")
+getip = gethostbyname(host)
+print ("[*] Target IP  :", getip)
+print ("Hapyy Hunting !!! :D")
